465aa15adbc68ce7b22493224f51aba85e761fcd
vijay <vjvijaykumarvj266@gmail.com>
2022-09-05 09:44:46 +0530

    function prime added

--- natural.py
+++ natural.py
@@ -1,12 +1,15 @@
-a = int(input('enter number'))
-b = int(input('enter number'))
-for i in range(a,b+1):
-    if i >1:
-        for j in range(2,i):
-            if i%j == 0:
-                break
-        else:
-            print(i)
+def checkin():
+    a = int(input('enter number'))
+    b = int(input('enter number'))
+    for i in range(a,b+1):
+        if i >1:
+            for j in range(2,i):
+                if i%j == 0:
+                    break
+            else:
+                print(i)
+checkin()
+
 
 
 
