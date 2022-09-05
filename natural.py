e025127207d1814be77cdd636c999f87a9b1e03f
vijay <vjvijaykumarvj266@gmail.com>
2022-09-05 08:45:43 +0530

    natural prime numbers added

--- natural.py
+++ natural.py
@@ -1,12 +1,12 @@
-a = int(input('enter number'))
-if a>1:
-    for i in range(2,a):
-        if a%i == 0:
-            print(a,'------>not prime number')
-            break
-    else:
-        print(a,'------>Prime number')
-
+a  =int(input('enter number'))
+b  =int(input('enter number'))
+for i in range(a,b+1):
+    if i >1:
+        for j in range(2,i):
+            if i%j == 0:
+                break
+        else:
+            print(i)
 
 
 
